becd0493c1c520992998afe28798d8d980d6386e
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-04 21:01:33 +0700

    tambah DestinasiDetail dengan route, argumen itemId, dan title resource

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiNavigasi.kt
@@ -16,3 +16,10 @@ object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
